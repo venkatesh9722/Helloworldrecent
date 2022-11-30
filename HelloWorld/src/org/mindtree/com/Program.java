10eb14bdfa780510f1693e898285a11291f9a05c
venkatesh9722 <venki9282@gmail.com>
2022-11-30 09:06:57 +0530

    initial commit

---
+++ HelloWorld/src/org/mindtree/com/Program.java
@@ -0,0 +1,14 @@
+package org.mindtree.com;
+
+public class Program {
+
+	
+	    public static void main(String[] args) {
+	        Test();
+	        }
+	    public static void Test(){
+	        System.out.println("Hello World!");
+	    }
+	
+
+}
